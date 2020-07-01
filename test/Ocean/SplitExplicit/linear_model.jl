c48e8804a62e5c193576343c0ec879fdad72ad54
christophernhill <christophernhill@gmail.com>
2020-07-01 10:44:20 -0400

    Switch to stand alone model

--- test/Ocean/SplitExplicit/linear_model.jl
+++ test/Ocean/SplitExplicit/linear_model.jl
@@ -23,6 +23,7 @@
 
 # Create a new child linear model instance, attached to whatever parent
 # BalanceLaw instantiates this.
+# (No longer sure we need parent, but maybe we will get some parameters from it)
 struct om_ivdc_lm{M} <: BalanceLaw
   parent_om::M
   function om_ivdc_lm{parent_om::M} where {M}
@@ -35,60 +36,53 @@ end
  Map child model state to parent variables
 """
 
-# Base state variables, u, η, θ
-# Do I really want u and η hanging around, it seems like they may get computed on too
-# in the gradient kernels?
-# Do I really wany to inherit from parent - probably not I think.
-# Somewhere I need to iterate on a θ^{n+1}. It seems like doing that in the main θ could be annoying
-vars_state_conservative(lm:om_ivdc_lm, FT) = 
-    vars_state_conservative(lm.parent_om, FT)
+# State variable, just one for now, θ
+vars_state_conservative(lm:om_ivdc_lm, FT) = @vars(θ::FT)
 
-# Gradient variables, u, ud, θ (velocity is split into two versions one holding full (u) and one 
-# holding deviation from barotropic (ud) )
-# Do I really want to reuse G.∇θ here. I guess that saves memory, but it could be confusing.
-vars_state_gradient(lm:om_ivdc_lm, FT) = 
-    vars_state_gradient(lm.parent_om, FT)
+# "Gradiented" variables, θ
+vars_state_gradient(lm:om_ivdc_lm, FT) = @vars(θ::FT)
 
-# Intermediate first order flux variables, ν∇u and κ∇θ
-vars_state_gradient_flux(lm::om_ivdc_lm, FT) =
-    vars_state_gradient_flux(lm.parent-om, FT)
+# Intermediate first order flux variables, κ∇θ
+vars_state_gradient_flux(lm::om_ivdc_lm, FT) = @vars(κ∇θ::SVector{3, T})
 
 # All other vars_() can be empty for this linear model
 vars_state_auxiliary(lm::om_ivdc_lm, FT) = @vars()
 vars_integrals(lm::om_ivdc_lm, FT) = @vars()
 
 """
-    compute_gradient_argument!(::om_ivdclm)
+    compute_gradient_argument!(::om_ivdc_lm)
 
 copy θ to var_gradient
 this computation is done pointwise at each nodal point
 
 # arguments:
 - `m`: model in this case HBModel
-- `G`: array of gradient variables
+- `G`: array of gradient variables (to be computed in a kernel)
 - `Q`: array of state variables
 - `A`: array of aux variables
 - `t`: time, not used
 """
 @inline function compute_gradient_argument!(
     m::om_ivdc_lm,
-    G::Vars,
+    G!::Vars,
     Q::Vars,
     A,
     t,
 )
-    G.∇θ = Q.θ
-
-#   What about G.∇η and G.∇u? Do these need to be set to zero?
+    G!.θ = Q.θ
 
     return nothing
 end
 
 
+"""
+    compute_gradient_flux!(::om_ivdc_lm)
 
+copy gradient of θ to variable to be differentiated
+"""
 @inline function compute_gradient_flux!(
     m::om_ivdc_lm,
-    D::Vars,
+    D!::Vars,
     G::Grad,
     Q::Vars,
     A::Vars,
@@ -96,10 +90,27 @@ end
 )
 
     κ = diffusivity_tensor(m, G.θ[3])
-    D.κ∇θ = κ * G.θ
+    D!.κ∇θ = κ * G.θ
 
     return nothing
 end
+
+@inline function source!(
+    m::om_ivdc_lm
+    S!::Vars,
+    Q::Vars,
+    D::Vars,
+    A::Vars,
+    t::Real,
+    direction,
+)
+    @inbounds begin
+     S!.θ=Q.θ/m.parent_om.dt_slow
+    end
+
+    return nothing
+end
+
 
 @inline function diffusivity_tensor(m::om_ivdc_lm, ∂θ∂z)
   # ∂θ∂z < 0 ? κ = (@SVector [m.κʰ, m.κʰ, 1000 * m.κᶻ]) : κ =
@@ -110,7 +121,10 @@ end
 end
 
 # Need to add something to control hidden numerical flux computations
+# and penalty function
 
 # What about bc's - these are zero flux, so probably OK.
+
+# What about 3d mesh but 1d problem?
 
 
